2324a7635fb0fdb0361e87422f8636c3bf461768
root <root@runnervmeorf1.muhvirrxgieepmboiwwwvpzrwa.phxx.internal.cloudapp.net>
2026-05-15 03:55:56 +0000

    fix: promote/demote use m.mentionedJid instead of raw contextInfo

--- plugins/group-demote.js
+++ plugins/group-demote.js
@@ -4,7 +4,7 @@ export default async ({ sock, m, args, isOwner }) => {
 
   let users = []
   if (m.quoted) users.push(m.quoted.sender)
-  if (m.msg?.contextInfo?.mentionedJid?.length) users.push(...m.msg.contextInfo.mentionedJid)
+  if (m.mentionedJid?.length) users.push(...m.mentionedJid)
   for (const a of args) {
     const n = a.replace(/[^0-9]/g, '')
     if (n) users.push(n + '@s.whatsapp.net')
