5640ae24883e306f4e0665e68318b4b2fc0600e0
root <root@runnervmeorf1.teyjlkhzkogudgnizfo5inqstd.bx.internal.cloudapp.net>
2026-05-15 13:10:25 +0000

    remove .tourl from menu

--- plugins/tools-menu.js
+++ plugins/tools-menu.js
@@ -34,8 +34,7 @@ export default async ({ sock, m }) => {
             { items: ['.sticker / .s', 'Convert image/video to sticker'] },
             { items: ['.toimg / .toimage', 'Convert sticker to image'] },
             { items: ['.tomp3', 'Convert video/audio to MP3'] },
-            { items: ['.tovn', 'Convert audio to voice note'] },
-            { items: ['.tourl / .upload', 'Upload media to catbox/0x0/tmpfiles'] }
+            { items: ['.tovn', 'Convert audio to voice note'] }
           ]
         }
       },
